d89308b0184d451ad2009bee3000e3c964991434
Uladzimir-Myliarchyk <valodya_93@mail.ru>
2025-07-27 16:35:57 +0300

    added adaptive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,27 @@
 export function Header() {
   console.log("header");
 }
+
+export const header = document.createElement('header');
+const logo = document.createElement('a');
+logo.classList.add('logoHeader')
+const inputSearch = document.createElement('input');
+inputSearch.classList.add('searchInput');
+inputSearch.placeholder = 'Найти на Wildberries';
+const btnBasket = document.createElement('button');
+btnBasket.classList.add('btnBasket');
+btnBasket.innerHTML = 'Корзина';
+
+header.append(logo);
+header.append(inputSearch);
+header.append(btnBasket);
+
+export const tabBar = document.createElement('div');
+tabBar.classList.add('tabBar');
+const btnHome = document.createElement('button');
+btnHome.classList.add('tabBar__home');
+const btnBasketTab = document.createElement('button');
+btnBasketTab.classList.add('tabBar__basket');
+
+tabBar.append(btnHome);
+tabBar.append(btnBasketTab);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { ProductCard } from "./components/ProductCard.js";
-import { Header } from "./components/Header.js";
+import { header, tabBar, Header } from "./components/Header.js";
 import "./styles/style.css";
+
+const root = document.getElementById('app');
+root.append(header);
+root.append(tabBar);
+
 
 ProductCard();
 Header();
