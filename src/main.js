3ab034a1c937508b5490d28aee6684e6da5138d8
Veres-t <veres.tatianka@gmail.com>
2025-07-28 20:54:24 +0300

    feature: create basic card

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { ProductCard } from "./components/ProductCard.js";
+import { ProductCard, createCard} from "./components/ProductCard.js";
 import { Header } from "./components/Header.js";
 import "./styles/style.css";
 
 ProductCard();
 Header();
+createCard();
