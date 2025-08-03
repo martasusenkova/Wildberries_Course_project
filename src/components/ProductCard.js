e1c4f11c89e8b97fcf29b1faa9ac62f7a85223f6
Veres-t <veres.tatianka@gmail.com>
2025-08-03 21:08:52 +0300

    feat: change button style

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -30,12 +30,17 @@ export function createCard(products) {
       const cardButton = document.createElement("button");
       cardButton.classList.add("card__button");
       card.appendChild(cardButton);
+      cardButton.addEventListener("click", () => changeText(cardButton));
+      function changeText(card__button) {
+      cardButton.textContent = "В корзине!";
+      cardButton.classList.add("card__button-two");
+      }
       const cardShowButton = document.createElement("button");
       cardShowButton.classList.add("card__show-button");
       card.appendChild(cardShowButton);
       cardShowButton.innerText = "Быстрый просмотр";
       cardButton.innerHTML =
-        '<svg class="card__icon" width="17" height="16" fill="#A73AFD" xmlns="http://www.w3.org/2000/svg"> <path class="card__icon-path" d="M2.925.488a.833.833 0 0 0-1.517.691l4.295 9.416v.001c.005.008.023.05.046.09a.9.9 0 0 0 .979.446c.045-.01.089-.023.098-.026l6.22-1.853.105-.031c.44-.13.867-.256 1.201-.523.29-.232.517-.535.657-.88.16-.396.159-.842.158-1.3V4.105c0-.01 0-.06-.004-.11a.901.901 0 0 0-.488-.73.9.9 0 0 0-.447-.098H4.147L2.925.487ZM11.833 12a1.333 1.333 0 0 0 0 2.667h.007a1.333 1.333 0 0 0 0-2.667h-.007ZM3.167 13.334c0-.737.597-1.334 1.333-1.334h.007a1.333 1.333 0 0 1 0 2.667H4.5a1.333 1.333 0 0 1-1.333-1.333Z" fill="#A73AFD"/></svg> <p>Корзина</p>'
+        '<svg class="card__icon" width="17" height="16" fill="#A73AFD" xmlns="http://www.w3.org/2000/svg"> <path class="card__icon-path" d="M2.925.488a.833.833 0 0 0-1.517.691l4.295 9.416v.001c.005.008.023.05.046.09a.9.9 0 0 0 .979.446c.045-.01.089-.023.098-.026l6.22-1.853.105-.031c.44-.13.867-.256 1.201-.523.29-.232.517-.535.657-.88.16-.396.159-.842.158-1.3V4.105c0-.01 0-.06-.004-.11a.901.901 0 0 0-.488-.73.9.9 0 0 0-.447-.098H4.147L2.925.487ZM11.833 12a1.333 1.333 0 0 0 0 2.667h.007a1.333 1.333 0 0 0 0-2.667h-.007ZM3.167 13.334c0-.737.597-1.334 1.333-1.334h.007a1.333 1.333 0 0 1 0 2.667H4.5a1.333 1.333 0 0 1-1.333-1.333Z" fill="#A73AFD"/></svg> <p>Корзина</p>';
       container.appendChild(card);
     });
   }
