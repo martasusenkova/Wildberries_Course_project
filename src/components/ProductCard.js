f67f100d7e661d89048cc3b4e887d276d63660e7
ivanezzz87 <mseptk@yandex.ru>
2025-08-03 23:10:18 +0300

    fix: fix productcard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,22 +1,4 @@
 import { openModalWindow } from "./ProductCardModal.js";
-export function ProductCard() {
-  console.log("Карточка товара");
-  const container = document.createElement("div")
-  container.classList.add("productContainer")
-  const app = document.getElementById("app")
-  console.log(app);
-  app.appendChild(container);
-  const card = document.createElement("div")
-  card.classList.add("card");
-  container.appendChild(card);
-  const cardShowButton = document.createElement("button")
-  cardShowButton.classList.add("card__show-button")
-  card.appendChild(cardShowButton)
-  cardShowButton.innerText = 'Быстрый просмотр';
-  cardShowButton.onclick = function () {
-      openModalWindow("");
-  }
-}
 export const product = {
   name: "Кроссовки",
   image:
