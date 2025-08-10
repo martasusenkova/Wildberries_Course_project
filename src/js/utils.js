7abdd446673370b9d4da921b47e7d860312a8bd8
Veres-t <veres.tatianka@gmail.com>
2025-08-10 21:24:55 +0300

    feat:added scroll button

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,3 +2,25 @@ export function getCard (id) {
     const cards = JSON.parse(localStorage.getItem('cards'));
     return cards.find(item => item.id === id);
 }
+
+
+const scrollToTopBtn = document.createElement("button");
+scrollToTopBtn.classList.add("scrollToTopBtn");
+app.appendChild(scrollToTopBtn);
+
+// Когда пользователь прокручивает вниз 50px от верха страницы, показываем кнопку
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+// Когда пользователь нажимает на кнопку, прокручиваем страницу вверх
+scrollToTopBtn.onclick = function() {
+  document.body.scrollTop = 0; // Для Safari
+  document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+}
